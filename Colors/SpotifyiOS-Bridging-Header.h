635fa195973cedcea9e08430f41315fe5b95cf9e
Hiromu Nakano <hiromu.nakano.12@gmail.com>
2020-04-06 18:54:39 +0900

    Add framework "SpotifyiOS"

---
+++ Colors/SpotifyiOS-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  SpotifyiOS-Bridging-Header.h
+//  Colors
+//
+//  Created by Hiromu Nakano on 2020/04/06.
+//  Copyright © 2020 Hiromu Nakano. All rights reserved.
+//
+
+#import <SpotifyiOS/SpotifyiOS.h>
